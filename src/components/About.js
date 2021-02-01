ca87d30dcf522360101ebd155df0ee7fc9a09ffc
Jason Zhou <jzhou35@buffalo.edu>
2021-02-01 14:06:26 -0500

    commit tree aef191af2b2c8a98d9e173d0c01a0d43d0b02dc9

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quam omnis nisi excepturi illo quidem ipsum cupiditate doloribus autem. Fugit possimus suscipit ratione quos? Maiores est perferendis eligendi sit temporibus.</p>
+    <img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
